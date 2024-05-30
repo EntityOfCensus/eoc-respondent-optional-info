2785422526dd374db11f018aa9d459d63a6a3f1d
Octavian Stirbei <octavianstirbei@gmail.com>
2024-05-31 00:43:31 +0300

    setup openapi, types and permaweb aoconnect

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,33 @@ export const Task = {
 	completed: Boolean,
 	due_date: new DateTime(),
 };
+
+export const Question = {
+	question: String,
+	questionType: String,
+    category: String,
+	possibleAnswers: Array<String>,
+	answers: Array<String>	
+};
+
+
+export const TargetGroup = {
+	age: Number,
+	gender: String,
+    country: String,
+	surveyData: Array<typeof Question>
+};
+
+export const NewRespondentProfileSurvey = {
+	type: String,
+	respondentOthentSub: String,
+    targetGroups: Array<typeof TargetGroup> 
+};
+
+export const RespondentProfileSurvey = {
+	id: String,
+	type: String,
+	respondentOthentSub: String,
+    targetGroups: Array<typeof TargetGroup> 	
+};
+
